7bfbf2b7f8a6c3fe7032119bcdf64a26640ef548
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-20 15:39:23 +0200

    [add] idnumber on square and change color on click

--- js/script.js
+++ js/script.js
@@ -3,37 +3,44 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 
-//1. generare una griglia dinamicamente ( quindi imposto quanti quadrati devo mettere su una riga [10])
-//2 creo l'elemento square in modo dinamico con un ciclo.
-//3. numero i quadrati con una funzione da min a max [1 a 100]
-//4. ogni casella deve emettere il suo valore .this in console
-//5. la casella cliccata si colora
-
 const container = document.querySelector('.container');
 const squareForRow = 10;
 
-//salvo il numero massimo dei quadratini
+//salvo il numero dei quadratini
 const totalSquare = squareTot(squareForRow);
-const squares = squareGen(totalSquare)
+//salvo il numero totale dei quadratini
+const squares = squareGen(totalSquare);
 
-console.log(maxSquare);
 
 
 /**
  * Calcola il numero massimo dei quadratini
  * @param {number} numberSquareForRow 
- * @returns 
+ * @returns totale Square
  */
 function squareTot(numberSquareForRow){
   return totSquare = Math.pow(numberSquareForRow, 2);
 }
 
+/**
+ * Genera il numero di quadrati necesari e lo stampa in HMTL facendo cambiare colore al click della casella
+ * @param {number} nSquare 
+ * @returns square stampati in html
+ */
 function squareGen(nSquare){
+  let square;
   for(let i = 0; i < nSquare; i++){
-    const square = document.createElement('div');
+    let square = document.createElement('div');
     square.className = "square";
     container.append(square);
+    square.idSq = i + 1;
+    square.addEventListener('click', function(){
+      square.innerText = i + 1;
+      square.classList.add('color')
+      console.log(this.idSq);
+    })
   }
-
-  return square;
+  return;
 }
+
+
