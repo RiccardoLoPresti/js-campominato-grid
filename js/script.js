f0a372d6843afc18dd08d7d34fbc6e485f17f8c0
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-20 17:47:36 +0200

    [removed] comment and change text color square

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-/*L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
 const btn = document.getElementById('play')
 const container = document.querySelector('.container');
 let squareForRow = 0;
@@ -38,7 +34,6 @@ function squareTot(numberSquareForRow){
  * @returns square stampati in html
  */
 function squareGen(nSquare){
-
   for(let i = 0; i < nSquare; i++){
     let square = document.createElement('div');
     square.className = "square";
